d684f41bcf78b8f3251935823f09fb2df2475e2b
Cade <cadeburkett@Cades-MBP.attlocal.net>
2021-02-28 16:26:52 -0800

    created server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./public"));
+app.use(morgan("dev"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker-", {
+
+});
+
+//routes
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+});
